4f3a77851e31151ccb225a717c18455c3779a2d0
buffyhridoy <chemicalbuffy@gmail.com>
2022-09-17 16:48:02 +0600

    core concepts done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const products = [
+  { name: 'Photoshop', price: '$90.99' },
+  { name: 'Illustrator', price: '$60.99' },
+  { name: 'PDF Reader', price: '$6.99' },
+  { name: 'Premiere El', price: '$249.99' }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter></Counter>
+        <Users></Users>
+        {
+          products.map(pd => <Product product={pd}></Product>)
+
+        }
+
+        {
+          products.map(pd => <li>{pd.name}</li>)
+        }
+
       </header>
     </div>
   );
+}
+
+function Product(props) {
+
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+
+  const { name, price } = props.product;
+
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onMouseMove={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>decrease</button>
+    </div>
+  )
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
